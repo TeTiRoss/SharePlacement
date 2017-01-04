d54de01d48d93ddcbfb1c7a794e614827f250ed0
TeTiRoss <rostyk.s@hmarynka.com>
2017-01-04 20:03:19 +0200

    model testing

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Placement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:address) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:photos) }
 end
